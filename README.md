482ab191720ec4096bba585e026ec54ed1b2135d
Jatin <jatin8085vashisht@gmail.com>
2022-04-10 11:39:30 +0530

    jwt authentication working

--- README.md
+++ README.md
@@ -23,6 +23,9 @@
 
 ![client-server-interaction](/assets/client-server-interaction.png)
 
+## Password Validation Process
+![password-validation-process](/Chapter6_Intro_to_passport_JWT_authentication/assets/password-validation-process.png)
+
 ## Ways to implement JWT authentication in our app
 ### Most Complext
 - Use the Node JS crypto library and wirte our own middleware to sign and verify JWTs
@@ -38,3 +41,48 @@
 - On every HTTP request that requires authentication, the user client (browser) will attach the JWT in the `Authorization` HTTP header and verifies the signature
 - If the signature is valid, the server dcodes the JWT, usually gets the database ID of the user in the `payload.sub` field, looks the user up in the database, and stores the use object to sue.
 - The user receives the route data
+
+## Passport configuration
+- make a package/folder named config and create a passport.js file
+- below is the most important code snippet of file
+```
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    // we are using public key rather than private
+    // because we are configuring verify peice of JWT
+    secretOrKey: PUB_KEY,
+    algorithms: ['RS256']
+};
+
+// we have to define our strategy
+// it is much less complex than session way of authenticating user
+// payload will contain the meta data of the user
+const strategy = new JwtStrategy(options, (payload, done) => {
+    
+    User.findOne({ _id: payload.sub })
+        .then((user) => {
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch(err => {
+            done(err, null)
+        })
+});
+
+module.exports = (passport) => {
+    passport.use(strategy)
+}
+```
+
+### Protecting the routes
+```
+// we are not using sesion because we are using jwt
+// we have to write this or similar to this logic to every route that we want to protect
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    // will land user here if user is valid
+    res.status(200).json({success: true, msg: "you are authorized"})
+});
+```
--- express-jwt-authentication-starter/routes/users.js
+++ express-jwt-authentication-starter/routes/users.js
@@ -1,17 +1,60 @@
 const mongoose = require('mongoose');
-const router = require('express').Router();   
+const router = require('express').Router();
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
-// TODO
-router.get('/protected', (req, res, next) => {
+// we are not using sesion because we are using jwt
+// we have to write this or similar to this logic to every route that we want to protect
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    // will land user here if user is valid
+    res.status(200).json({success: true, msg: "you are authorized"})
+});
+
+router.post('/login', function (req, res, next) {
+    User.findOne({ username: req.body.username })
+        .then((user) => {
+            if (!user) {
+                res.status(401).json({ success: false, msg: "could not find user" })
+            }
+            const isValid = utils.validPassword(req.body.password, user.hash, user.salt)
+
+            if (isValid) {
+                const tokenObject = utils.issueJWT(user);
+
+                res.status(200).json({ success: true, user: user, token: tokenObject.token, expiresIn: tokenObject.expires })
+            } else {
+                res.status(401).json({ success: false, msg: "you entered the wrong password" })
+            }
+
+        })
+        // make sure to make an error handler to work below snippet work
+        .catch((err) => {
+            next(err)
+        })
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/register', function (req, res, next) {
+    // getting plain text password
+    const saltHash = utils.genPassword(req.body.password);
 
-// TODO
-router.post('/register', function(req, res, next){});
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    })
+
+    newUser.save()
+        .then((user) => {
+            // issueJWT will grab id from user object
+            const jwt = utils.issueJWT(user)
+            res.json({ success: true, user: user, token: jwt.token, expiresIn: jwt.expires })
+        })
+        .catch(err => next(err))
+});
 
 module.exports = router;
